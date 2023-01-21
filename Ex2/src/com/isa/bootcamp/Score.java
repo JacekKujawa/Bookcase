d6d3927b4cd2656967fb8fe906a85b4642b5ff8f
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-01-21 13:07:15 +0100

    Score class Getter and Setter added

--- Ex2/src/com/isa/bootcamp/Score.java
+++ Ex2/src/com/isa/bootcamp/Score.java
@@ -1,4 +1,30 @@
 package com.isa.bootcamp;
 
 public class Score {
+
+    private int scoreNumber;
+    private String feedback;
+
+    public Score(int scoreNumber, String feedback) {
+        this.scoreNumber = scoreNumber;
+        this.feedback = feedback;
+    }
+
+    public int getScoreNumber() {
+        return scoreNumber;
+    }
+
+    public void setScoreNumber(int scoreNumber) {
+        this.scoreNumber = scoreNumber;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+
 }

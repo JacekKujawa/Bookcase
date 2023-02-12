14663ab196cda497acec8d7d8b7a84c8d224696d
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-02-12 09:20:41 +0100

    Draw class code add

--- Ex3/src/main/java/com/isa/lotto/Draw.java
+++ Ex3/src/main/java/com/isa/lotto/Draw.java
@@ -1,5 +1,33 @@
 package com.isa.lotto;
 import java.util.Random;
 public class Draw {
-
+    int k = 0;
+    private int[] array;
+    private int[] resultArray;
+    public void arrayMake(int n) {
+        this.array = new int[n];
+        this.resultArray = new int[n];
+    }
+    public void generate(int n) {
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(n) + 1;
+            boolean exists = false;
+            for (int j = 0; j < i; j++) {
+                if (array[i] == array[j]) {
+                    exists = true;
+                    --i;
+                }
+            }
+            if (!exists) {
+                resultArray[k] = array[i];
+                k++;
+            }
+        }
+    }
+    public void write() {
+        for (int i = 0; i < k; i++) {
+            System.out.println(resultArray[i] + " ");
+        }
+    }
 }

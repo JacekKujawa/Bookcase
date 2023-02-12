b9ed73a1fcf3e099c73d5267ad8420e529fc1a17
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-02-12 09:18:35 +0100

    ReadNumber class code add

--- Ex3/src/main/java/com/isa/lotto/ReadNumber.java
+++ Ex3/src/main/java/com/isa/lotto/ReadNumber.java
@@ -1,8 +1,30 @@
 package com.isa.lotto;
 
-
 import java.util.Scanner;
 
 public class ReadNumber {
+    private int range;
+    private int quantity;
 
+    public void data() {
+        Security s1 = new Security();
+        do {
+            // Make new object d1,s1,scanner
+            Scanner scanner = new Scanner(System.in);
+            // Read range and quantity
+            System.out.println("Podaj z jakiego zakresu mam wylosować liczby: ");
+            range = scanner.nextInt();
+            System.out.println("Podaj ile liczb mam wylosować:");
+            quantity = scanner.nextInt();
+            s1.security(range, quantity);
+        } while (s1.isS());
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }

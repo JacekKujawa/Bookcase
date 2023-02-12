10f0753d7454e5e70a62f0fd4771a917a55428e3
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-02-12 09:19:52 +0100

    Security class code add

--- Ex3/src/main/java/com/isa/lotto/Security.java
+++ Ex3/src/main/java/com/isa/lotto/Security.java
@@ -3,4 +3,22 @@ package com.isa.lotto;
 import java.sql.Struct;
 
 public class Security {
+    private boolean s;
+
+    public void security(int x, int y) {
+
+        if (y > 0 && x >= y) {      // is the number positive?
+            System.out.println("Podane wartości " + x + " i " + y + " są prawidłowe :)");
+            s = false;
+        } else { //if the number is not positive?
+            System.out.println("Błąd !!! Podane wartości " + x + " i " + y + " muszą być dodatnie oraz zakres większy od ilości liczb:)");
+            System.out.println("Podaj liczby jeszcze raz:)");
+            s = true;
+        }
+    }
+
+    public boolean isS() {
+        return s;
+    }
+
 }

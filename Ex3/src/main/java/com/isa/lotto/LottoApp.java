798702657e3357a2ae13030590faceea78971a43
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-02-12 09:21:31 +0100

    LottoApp class code add

--- Ex3/src/main/java/com/isa/lotto/LottoApp.java
+++ Ex3/src/main/java/com/isa/lotto/LottoApp.java
@@ -1,7 +1,16 @@
 package com.isa.lotto;
 
-
 public class LottoApp {
-
+    public static void main(String[] args) {
+        ReadNumber n1 = new ReadNumber();
+        Draw d1 = new Draw();
+        n1.data();
+        //make array
+        d1.arrayMake(n1.getQuantity());
+        //generate numbers
+        d1.generate(n1.getRange());
+        System.out.println("wylosowane liczby to:");
+        // write numbers in console
+        d1.write();
+    }
 }
-

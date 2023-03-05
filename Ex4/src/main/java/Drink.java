ecdee96c0b23dde14305c6e67d4e7ea924ed7a4d
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-03-05 14:58:48 +0100

    Enum structure add

--- Ex4/src/main/java/Drink.java
+++ Ex4/src/main/java/Drink.java
@@ -1,2 +1,4 @@
 public class Drink {
+    private String drink;
+
 }
---
+++ Ex4/src/main/java/Size.java
@@ -0,0 +1,5 @@
+public enum Size {;
+    private final String small = "25cm";
+    private final String middle = "30cm";
+    private final String family = "30cm";
+}

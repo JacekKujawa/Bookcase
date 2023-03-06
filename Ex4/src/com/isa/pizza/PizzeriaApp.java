f76f9ec8b0f87d33111d74a12bca85c6979e3f75
Jacek <jacek.kujawa0909@gmail.com>
2023-03-06 15:27:54 +0100

    PizzaApp class modify

--- Ex4/src/com/isa/pizza/PizzeriaApp.java
+++ Ex4/src/com/isa/pizza/PizzeriaApp.java
@@ -4,9 +4,11 @@ public class PizzeriaApp {
     public static void main(String[] args) {
         Order order1 = new Order();
         System.out.println(order1 + "\n");
+        System.out.println("Print hashCode: ");
         System.out.println(order1.getClient1().hashCode());
         System.out.println(order1.getClient2().hashCode());
         System.out.println(order1.getClient3().hashCode());
+        System.out.println("Print equals method: ");
         System.out.println(order1.getClient1().equals(order1.getClient2()));
     }
 }

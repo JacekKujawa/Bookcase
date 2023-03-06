837543df1cc4c63ff23c4a9dfe7f88b4005c9b04
Jacek <jacek.kujawa0909@gmail.com>
2023-03-06 15:32:13 +0100

    PizzaApp class fix print information

--- Ex4/src/com/isa/pizza/PizzeriaApp.java
+++ Ex4/src/com/isa/pizza/PizzeriaApp.java
@@ -5,10 +5,10 @@ public class PizzeriaApp {
         Order order1 = new Order();
         System.out.println(order1 + "\n");
         System.out.println("Print hashCode: ");
-        System.out.println(order1.getClient1().hashCode());
-        System.out.println(order1.getClient2().hashCode());
-        System.out.println(order1.getClient3().hashCode());
-        System.out.println("Print equals method: ");
+        System.out.println("Client1: "+order1.getClient1().hashCode());
+        System.out.println("Client2: "+order1.getClient2().hashCode());
+        System.out.println("Client3: "+order1.getClient3().hashCode());
+        System.out.println("Print equals method: (Client1 vs  Client2)");
         System.out.println(order1.getClient1().equals(order1.getClient2()));
     }
 }

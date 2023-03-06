0107c926b882184f2455fc50f471078b7b2f84a7
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-03-06 20:46:26 +0100

    Order class modify

--- Ex4/src/main/java/com/isa/pizzeria/Order.java
+++ Ex4/src/main/java/com/isa/pizzeria/Order.java
@@ -4,24 +4,25 @@ public class Order {
     Client client1 = new Client("Zenek", "Łąkowa 11E", "555-555-555");
     Client client2 = new Client("Krysia", "Nadrzeczna 6/3", "999-999-999");
     Client client3 = new Client("Antoni", "Polna 13", "777-777-777");
-    Pizza pizza1 = new Pizza(PizzaName.PEPPERONI, PizzaSize.FAMILY, 1);
-    Drink drink1 = new Drink(DrinkName.JUICE, 2);
-    Pizza pizza2 = new Pizza(PizzaName.DIAVIOLA, PizzaSize.SMALL, 1);
-    Pizza pizza2a = new Pizza(PizzaName.CAPRICCIOSA, PizzaSize.MIDDLE, 1);
-    Drink drink2 = new Drink(DrinkName.COLA, 1);
-    Hamburger hamburger3 = new Hamburger(1);
-    Drink drink3 = new Drink(DrinkName.BEER, 1);
+    Product pizza1 = new Pizza(PizzaName.PEPPERONI, PizzaSize.FAMILY, 1);
+    Product drink1 = new Drink(DrinkName.JUICE, 2);
+    Product pizza2 = new Pizza(PizzaName.DIAVIOLA, PizzaSize.SMALL, 1);
+    Product pizza2a = new Pizza(PizzaName.CAPRICCIOSA, PizzaSize.MIDDLE, 1);
+    Product drink2 = new Drink(DrinkName.COLA, 1);
+    Product hamburger3 = new Hamburger(1);
+    Product drink3 = new Drink(DrinkName.BEER, 1);
+
 
     @Override
     public String toString() {
         return
-                "\n" + "Client1=" + client1 +
+                "\n\n" + "Client1=" + client1 +
                         ", pizza1=" + pizza1 +
-                        ", drink1=" + drink1 + "\n" +
+                        ", drink1=" + drink1 + "\n\n" +
                         "Client2=" + client2 +
                         ", pizza2=" + pizza2 +
                         ", pizza2a=" + pizza2a +
-                        ", drink2=" + drink2 + "\n" +
+                        ", drink2=" + drink2 + "\n\n" +
                         "Client3=" + client3 +
                         ", hamburger3=" + hamburger3 +
                         ", drink3=" + drink3;
@@ -38,5 +39,6 @@ public class Order {
     public Client getClient3() {
         return client3;
     }
+
 }
 
--- Ex4/src/main/java/com/isa/pizzeria/PizzeriaApp.java
+++ Ex4/src/main/java/com/isa/pizzeria/PizzeriaApp.java
@@ -1,11 +1,9 @@
 package com.isa.pizzeria;
 
-import com.isa.pizzeria.Order;
-
 public class PizzeriaApp {
     public static void main(String[] args) {
         Order order1 = new Order();
-        System.out.println(order1 + "\n");
+        System.out.println("Order1: " + order1 + "\n");
         System.out.println("Print hashCode: ");
         System.out.println("Client1: "+order1.getClient1().hashCode());
         System.out.println("Client2: "+order1.getClient2().hashCode());

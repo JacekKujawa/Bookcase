f35de1ab48bebf1c48dbe527f956a9e503650226
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-03-18 17:20:32 +0100

    Maven structure add

---
+++ Ex5/src/main/java/com/isa/quiz/QuizApp.java
@@ -0,0 +1,13 @@
+package com.isa.quiz;
+
+/**
+ * Hello world!
+ *
+ */
+public class QuizApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

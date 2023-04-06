beb882c25ffdf60432309f6d83be3c97f2deb8e6
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-04-06 13:13:19 +0200

    Book, Books class and enum Category add

--- Ex6/src/main/java/com/isa/bookcase/App.java
+++ Ex6/src/main/java/com/isa/bookcase/App.java
@@ -1,4 +1,4 @@
-package com.isa;
+package com.isa.bookcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Ex6/src/test/java/com/isa/bookcase/AppTest.java
+++ Ex6/src/test/java/com/isa/bookcase/AppTest.java
@@ -1,4 +1,4 @@
-package com.isa;
+package com.isa.bookcase;
 
 import junit.framework.Test;
 import junit.framework.TestCase;

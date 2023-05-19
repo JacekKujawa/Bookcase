35ac58223caa53bae393410144e25713d0e1b02e
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-05-19 15:44:52 +0200

    TaskServiceTest class added

---
+++ Ex7/src/test/java/com/isa/todo/service/TaskServiceTest.java
@@ -0,0 +1,64 @@
+package com.isa.todo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void findTasksWithPriority1() {
+    }
+
+    @Test
+    void findTasksForNextDay() {
+    }
+
+    @Test
+    void sortTasksByPriorityDescending() {
+    }
+
+    @Test
+    void sortTasksByDate() {
+    }
+
+    @Test
+    void removeTask() {
+    }
+
+    @Test
+    void findTasksByCategory() {
+    }
+
+    @Test
+    void findTasksByDescriptionContains() {
+    }
+
+    @Test
+    void findMostUrgentTask() {
+    }
+
+    @Test
+    void divideTasksByCategory() {
+    }
+
+    @Test
+    void divideTasksByPriority() {
+    }
+
+    @Test
+    void findHighestPriorityTaskForEachCategory() {
+    }
+}

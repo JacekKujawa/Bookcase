bcfc4528ba0f6b812af19eff35399c9604c0fc76
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-05-20 11:23:05 +0200

    sortTasksByDate_WhenTasksExist_ShouldReturnTasksSortedByDate and sortTasksByDate_WhenNoTasksExist_ShouldReturnEmptyList  test added

--- Ex7/src/test/java/com/isa/todo/service/TaskServiceTest.java
+++ Ex7/src/test/java/com/isa/todo/service/TaskServiceTest.java
@@ -213,8 +213,36 @@ class TaskServiceTest {
 
 
     @Test
-    void sortTasksByDate() {
+    void sortTasksByDate_WhenTasksExist_ShouldReturnTasksSortedByDate() {
+        // Given
+        Task task1 = new Task("Task 1", Category.WORK, 1, LocalDate.now().plusDays(2));
+        Task task2 = new Task("Task 2", Category.HOME, 3, LocalDate.now().plusDays(1));
+        Task task3 = new Task("Task 3", Category.OTHER, 2, LocalDate.now());
+
+        when(taskRepository.getAllTasks()).thenReturn(Arrays.asList(task1, task2, task3));
+
+        // When
+        List<Task> tasks = taskService.sortTasksByDate();
+
+        // Then
+        assertEquals(3, tasks.size());
+        assertEquals(task3, tasks.get(0));
+        assertEquals(task2, tasks.get(1));
+        assertEquals(task1, tasks.get(2));
     }
+
+    @Test
+    void sortTasksByDate_WhenNoTasksExist_ShouldReturnEmptyList() {
+        // Given
+        when(taskRepository.getAllTasks()).thenReturn(Collections.emptyList());
+
+        // When
+        List<Task> tasks = taskService.sortTasksByDate();
+
+        // Then
+        assertTrue(tasks.isEmpty());
+    }
+
 
     @Test
     void removeTask() {
